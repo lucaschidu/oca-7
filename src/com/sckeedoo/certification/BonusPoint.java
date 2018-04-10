11109b2b46434f1ca3fe99f727bc63ee2519e66a
lucaschidu <38067937+lucaschidu@users.noreply.github.com>
2018-04-10 16:03:49 +0300

    Update BonusPoint.java

--- src/com/sckeedoo/certification/BonusPoint.java
+++ src/com/sckeedoo/certification/BonusPoint.java
@@ -6,9 +6,9 @@ public class BonusPoint {
      *
      * You probably have used the trolleybus in Chisinau.
      * Every time you enter in trolleybus you have to get a ticked from the taxator otherwise you will be a iepurash
-     * Every ticket has a number of it.
+     * Every ticket has a number on it.
      * The ticket is considered to be lucky if the sum of it's first 3 digits are equal to the sum of it's 3 last digits
-     * Ex. of lucky ticket : 125800 - 1 + 2 + 4 == 8 + 0 + 0
+     * Ex. of lucky ticket : 125800 - 1 + 2 + 5 == 8 + 0 + 0
      * A super lucky ticket is considered if it's number is a palindrome (google what a palindrome means).
      *
      * Task - Given a ticket number, find out it it's lucky , super lucky or a simple ticket
